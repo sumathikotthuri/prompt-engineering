63dea3681ad2551a1584b3ee8ddf40f526b725d0
sumathikotthuri <kotthuri.sumathi@gmail.com>
2023-05-24 13:04:22 +0530

    Tried out different types of summary prompts

---
+++ summarize-prompts/extract-info.py
@@ -0,0 +1,45 @@
+import openai
+import os
+from decouple import Config, RepositoryEnv
+
+config = Config(RepositoryEnv("config.env"))
+openai.api_key  = config('OPENAI_API_KEY')
+#from IPython.display import display, HTML
+# Above statement you can use if wants to render in browser
+
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+prod_review = """
+Got this panda plush toy for my daughter's birthday, \
+who loves it and takes it everywhere. It's soft and \ 
+super cute, and its face has a friendly look. It's \ 
+a bit small for what I paid though. I think there \ 
+might be other options that are bigger for the \ 
+same price. It arrived a day earlier than expected, \ 
+so I got to play with it myself before I gave it \ 
+to her.
+"""
+
+prompt = f"""
+Your task is to extract relevant information from \ 
+a product review from an ecommerce site to give \
+feedback to the Shipping department. 
+
+From the review below, delimited by triple quotes \
+extract the information relevant to shipping and \ 
+delivery. Limit to 30 words. 
+
+Review: ```{prod_review}```
+"""
+
+response = get_completion(prompt)
+print(response)
---
+++ summarize-prompts/from-multiple-reviews.py
@@ -0,0 +1,83 @@
+import openai
+import os
+from decouple import Config, RepositoryEnv
+
+config = Config(RepositoryEnv("config.env"))
+openai.api_key  = config('OPENAI_API_KEY')
+#from IPython.display import display, HTML
+# Above statement you can use if wants to render in browser
+
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+review_1 = """
+Got this panda plush toy for my daughter's birthday, \
+who loves it and takes it everywhere. It's soft and \ 
+super cute, and its face has a friendly look. It's \ 
+a bit small for what I paid though. I think there \ 
+might be other options that are bigger for the \ 
+same price. It arrived a day earlier than expected, \ 
+so I got to play with it myself before I gave it \ 
+to her.
+"""
+
+# review for a standing lamp
+review_2 = """
+Needed a nice lamp for my bedroom, and this one \
+had additional storage and not too high of a price \
+point. Got it fast - arrived in 2 days. The string \
+to the lamp broke during the transit and the company \
+happily sent over a new one. Came within a few days \
+as well. It was easy to put together. Then I had a \
+missing part, so I contacted their support and they \
+very quickly got me the missing piece! Seems to me \
+to be a great company that cares about their customers \
+and products. 
+"""
+
+# review for an electric toothbrush
+review_3 = """
+My dental hygienist recommended an electric toothbrush, \
+which is why I got this. The battery life seems to be \
+pretty impressive so far. After initial charging and \
+leaving the charger plugged in for the first week to \
+condition the battery, I've unplugged the charger and \
+been using it for twice daily brushing for the last \
+3 weeks all on the same charge. But the toothbrush head \
+is too small. I’ve seen baby toothbrushes bigger than \
+this one. I wish the head was bigger with different \
+length bristles to get between teeth better because \
+this one doesn’t.  Overall if you can get this one \
+around the $50 mark, it's a good deal. The manufactuer's \
+replacements heads are pretty expensive, but you can \
+get generic ones that're more reasonably priced. This \
+toothbrush makes me feel like I've been to the dentist \
+every day. My teeth feel sparkly clean! 
+"""
+
+
+reviews = [review_1, review_2, review_3]
+
+
+for i in range(len(reviews)):
+    prompt = f"""
+    Your task is to generate a short summary of a product \ 
+    review from an ecommerce site. 
+
+    Summarize the review below, delimited by triple \
+    backticks in at most 20 words. 
+
+    Review: ```{reviews[i]}```
+    """
+
+    response = get_completion(prompt)
+    print(i, response, "\n")
+
